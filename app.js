94300f0dfe89e6ba4f7e0b07cf2e2b4cda0a341f
rlbertolini <rlbertolini@gmail.com>
2026-08-07 16:51:25 -0300

    Adiciona funcionalidades iniciais do aplicativo

---
+++ app.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const botoes = document.querySelectorAll("button");
+
+    botoes.forEach((botao) => {
+        botao.addEventListener("click", () => {
+
+            alert(
+                "🚧 Esta função será construída nas próximas aulas.\n\nBem-vindo ao EstudaIA Kids!"
+            );
+
+        });
+    });
+
+});
